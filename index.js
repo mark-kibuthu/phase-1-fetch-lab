d1226cc4e451f076114c134688dfecd1cc83811c
Agent-grey <markkibuthu@gmail.com>
2024-07-24 05:56:20 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
+    return fetch('https://anapioficeandfire.com/api/books')
+      .then(response => response.json()) // Convert response to JSON
+      .then(data => renderBooks(data));
 }
 
 function renderBooks(books) {
